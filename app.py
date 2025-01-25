0f5924bece549c3647fff005a5f572a67819ac17
saiakhil-bit <saiakhilambati7@gmail.com>
2025-01-25 20:54:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,8 +123,9 @@ def logout():
 # Initialize the database
 with app.app_context():
     db.create_all()
+if __name__ == "__main__":
+    from os import environ
+    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
 
